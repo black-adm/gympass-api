55287ba146dc71d5af1da9b4c16e633a11f005a4
black-adm <mattheuscontato17@gmail.com>
2023-11-25 09:15:37 -0300

    feat: created gym repository and in memory repository

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,40 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async findByMemberIdOnDate(memberId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.member_id === memberId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      member_id: data.member_id,
+      gym_id: data.gym_id,
+      validate_at: data.validate_at ? new Date(data.validate_at) : null,
+      created_at: new Date(),
+    }
+
+    this.items.push(checkIn)
+    return checkIn
+  }
+}
